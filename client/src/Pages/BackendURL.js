049a2e3abb739b7a027210792795bca85a88be73
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-07-28 00:04:23 +0530

    added home page

--- client/src/Pages/BackendURL.js
+++ client/src/Pages/BackendURL.js
@@ -1,1 +1,1 @@
-export const backend_url = "http://localhost:8080";
+export const backend_url = "https://circunetics.onrender.com";
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Box, Heading, Img } from '@chakra-ui/react';
 
 const Home = () => {
+
   return (
-    <div>Home</div>
-  )
+    <Box textAlign={"center"} display="flex" justifyContent={"center"} alignItems="center" flexDirection={"column"}>
+      <Heading mb="10px">Welcome to Task App</Heading>
+      <Img height={"400px"} w="500px" src='./logo.png' alt='logo profile' />
+    </Box>
+  );
 }
 
 export default Home;
